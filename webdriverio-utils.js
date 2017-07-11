9f9b849b0863c8d63fb611e41aefb380e848484f
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2017-07-11 15:11:00 +0200

    feat(webdriverio): provide promise support for asserting logs

--- webdriverio-utils.js
+++ webdriverio-utils.js
@@ -9,38 +9,47 @@ var logLevels = {
 
 var debugMode = false
 var debugLevel = logLevels.INFO.value
-function assertNoBrowserErrors (whitelist, done) {
-  // TODO: Bug in ChromeDriver: Need to execute at least one command
-  // so that the browser logs can be read out!
-  browser.execute('1+1')
-  browser.log('browser').then(function (response) {
-    var failureEntries = []
-    var debugLogs = []
-    var browserLog = response.value
+function assertNoBrowserErrors (whitelist) {
+  return new Promise((resolve, reject) => {
+    // TODO: Bug in ChromeDriver: Need to execute at least one command
+    // so that the browser logs can be read out!
+    browser.execute('1+1')
+    browser.log('browser')
+      .then(function (response) {
+        var failureEntries = []
+        var debugLogs = []
+        var browserLog = response.value
 
-    for (var i = 0; i < browserLog.length; i++) {
-      var logEntry = browserLog[i]
-      if (isLogEntryATestFailure(logEntry, whitelist)) {
-        failureEntries.push(logEntry)
-      }
-      if (logLevels[logEntry.level].value >= debugLevel) {
-        debugLogs.push(logEntry)
-      }
-    }
+        for (var i = 0; i < browserLog.length; i++) {
+          var logEntry = browserLog[i]
+          if (isLogEntryATestFailure(logEntry, whitelist)) {
+            failureEntries.push(logEntry)
+          }
+          if (logLevels[logEntry.level].value >= debugLevel) {
+            debugLogs.push(logEntry)
+          }
+        }
 
-    if (failureEntries.length > 0 || debugMode) {
-      console.log('------------> FailuresLogs')
-      console.log(failureEntries)
-      console.log('------------> debugLogs')
-      console.log(debugLogs)
-    }
+        if (failureEntries.length > 0 || debugMode) {
+          console.log('------------> FailuresLogs')
+          console.log(failureEntries)
+        }
 
-    expect(failureEntries.length).toEqual(0, 'Expected no errors in the browserLog but got ' + failureEntries.length + ' error(s)') // .because()
-    if (typeof done === 'function') {
-      done()
-    } else if (debugMode) {
-      console.log('done is not a function')
-    }
+        if (debugMode) {
+          console.log('------------> debugLogs')
+          console.log(debugLogs)
+        }
+
+        if (failureEntries.length > 0) {
+          reject(new Error('Expected no errors in the browserLog but got ' + failureEntries.length + ' error(s)'))
+        } else {
+          resolve()
+        }
+        // expect(failureEntries.length).toEqual(0, 'Expected no errors in the browserLog but got ' + failureEntries.length + ' error(s)') // .because()
+
+      }, (error) => {
+        reject(error)
+      })
   })
 }
 
@@ -60,9 +69,28 @@ function isLogEntryATestFailure (entry, whitelist) {
   return result
 }
 module.exports = {
-  allowSomeBrowserErrors: assertNoBrowserErrors,
+  allowSomeBrowserErrors: function allowSomeBrowserErrors (whitelist, done) {
+    var then
+    if (typeof done === 'function') {
+      assertNoBrowserErrors(whitelist)
+        .then(() => {
+          done()
+        })
+    } else {
+      console.log('done is not a function')
+      return assertNoBrowserErrors(whitelist)
+    }
+  },
   verifyNoBrowserErrors: function verifyNoBrowserErrors (done) {
-    return assertNoBrowserErrors([], done)
+    if (typeof done === 'function') {
+      assertNoBrowserErrors([])
+        .then(() => {
+          done()
+        })
+    } else {
+      console.log('done is not a function')
+      return assertNoBrowserErrors([])
+    }
   },
   handleError: function handleError (done) {
     return function (error) {
